792b70f5d2fdab1a56cafa1594957ba9f9a28ddf
mdsoponhossain <mdsoponhossain2388@gmail.com>
2024-02-14 20:49:42 +0600

    home page search functionality ---- done

--- src/models/checkout/checkoutModel.js
+++ src/models/checkout/checkoutModel.js
@@ -1,28 +1,34 @@
 const mongoose = require('mongoose');
 
 const schema = mongoose.Schema({
-    title: String,
-    email: String,
-    location: String,
-    property_status: String,
-    description: String,
-    property_image: String,
-    property_details: mongoose.Schema.Types.Mixed,
-    facilities: [String],
-    floor_plan: {
-      image: String
-    },
-    map: String,
-    author: {
-      name: String,
-      contact: String,
-      phone: String
-    }
+  title: String,
+  email: String,
+  location: String,
+  property_status: String,
+  description: String,
+  property_image: String,
+  type:String,
+  property_details: mongoose.Schema.Types.Mixed,
+  facilities: [String],
+  floor_plan: {
+    image: String
   },
-    {
-        versionKey: false
-    }
+  map: String,
+  author: {
+    name: String,
+    contact: String,
+    phone: String
+  }
+},
+  {
+    versionKey: false
+  }
 );
+
+schema.query.queryFilterFn = function (want = '', location = '',type='') {
+  console.log('type:',want,location,type)
+   return this.where({property_status:new RegExp(want,'i'),location: new RegExp(location,'i'),type: new RegExp(type,'i') })
+}
 
 
 const checkoutModel = new mongoose.model("checkoutCollection", schema);
--- src/routes/homeRoutes/checkoutRoute/checkoutRoute.js
+++ src/routes/homeRoutes/checkoutRoute/checkoutRoute.js
@@ -5,10 +5,10 @@ const checkoutRoute = express.Router();
 
 // post multiple data ;
 checkoutRoute.post('/checkout', async (req, res) => {
-  console.log("fdfdfd",req.body);
   try {
     const result = await checkoutModel(req.body).save();
     console.log('checkout/home data is inserted');
+    // console.log("fdfdfd",result);
     res.send(result).status(200)
   } catch (error) {
     console.log('failed to insert the checkout data');
@@ -21,7 +21,9 @@ checkoutRoute.post('/checkout', async (req, res) => {
 // get all checkout data;
 checkoutRoute.get('/checkout', async (req, res) => {
   try {
-    const result = await checkoutModel.find();
+    console.log(req.query.type)
+    const result = await checkoutModel.find().queryFilterFn(req.query.want, req.query.location, req.query.type);
+    console.log("fdfdfd",result);
     console.log('checkout/home data is founded');
     res.send(result).status(200)
   } catch (error) {
@@ -47,13 +49,13 @@ checkoutRoute.get('/checkout', async (req, res) => {
 checkoutRoute.get('/checkout/:id', async (req, res) => {
   try {
     const id = req.params.id
-    const result = await checkoutModel.findById({ _id:id});
-    // console.log(result);
+    const result = await checkoutModel.findById({_id:id});
+    console.log(result);
     console.log('checkout/home data is founded');
     res.send(result).status(200)
   } catch (error) {
-    console.log('failed to find the checkout data');
     res.send(error.message).status(500)
+    console.log('failed to find the checkout data');
   }
 });
 module.exports = checkoutRoute;
